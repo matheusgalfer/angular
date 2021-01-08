f6d91bf62e7e7c0df93436221e6dc77cf1fa57db
Matheus <matheusgalvao03@gmail.com>
2021-01-08 16:41:36 -0300

    primeiros passos com o Angular2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeuPrimeiroComponet } from './meu-primeiro/meu-primeiro.componet';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponet,
+    MeuPrimeiro2Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/meu-primeiro/meu-primeiro.componet.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'meu-primeiro-componet',
+    template: `
+        <p>Meu primeiro componet com Angular 2!</p>
+    `
+})
+
+export class MeuPrimeiroComponet {
+
+}
